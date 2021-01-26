ead03b199051b0fa4062a12cda62222e274d2736
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-01-26 19:32:02 +0530

    direct commit

---
+++ Basics/newline.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+void printNewLine(){
+    cout << "Geeks" << endl << "for" << endl << "Geeks";
+}
+
+int main()
+{
+    printNewLine();
+    return 0;
+}
