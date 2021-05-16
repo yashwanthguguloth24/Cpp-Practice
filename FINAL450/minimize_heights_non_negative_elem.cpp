04afaf7f9d4a7f98d3a8c696e2da35a026dd1b34
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-05-16 22:44:19 +0530

    Create minimize_heights_non_negative_elem.cpp

---
+++ FINAL450/minimize_heights_non_negative_elem.cpp
@@ -0,0 +1,65 @@
+// https://practice.geeksforgeeks.org/problems/minimize-the-heights3351/1#
+
+// solution refernce - https://www.youtube.com/watch?v=X2TufR5vY98
+
+// code for non-negative elements
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        vector<pair<int,int>> poss_ele;
+        vector<int> visited(n);
+        for(int i = 0;i<n;i++)
+        {
+            if(arr[i]>=k)
+            {
+                poss_ele.push_back({arr[i]-k,i});
+            }
+            poss_ele.push_back({arr[i]+k,i});
+        }
+        sort(poss_ele.begin(),poss_ele.end());
+        int num_ele = 0;
+        int right = 0;
+        int left = 0;
+        while(num_ele < n && right < poss_ele.size())
+        {
+            if(visited[poss_ele[right].second] == 0)
+            {
+                num_ele += 1;
+            }
+            visited[poss_ele[right].second]++;
+            right++;
+        }
+        int res = poss_ele[right-1].first-poss_ele[left].first;
+        // moving window to right
+        while(right < poss_ele.size())
+        {
+            if(visited[poss_ele[left].second] == 1)
+            {
+                num_ele--;
+            }
+            visited[poss_ele[left].second]--;
+            left++;
+            while(num_ele < n && right < poss_ele.size())
+            {
+                if(visited[poss_ele[right].second] == 0)
+                {
+                    num_ele += 1;
+                }
+                visited[poss_ele[right].second]++;
+                right++;
+            }
+            if(num_ele == n)
+            {
+                res = min(res,poss_ele[right-1].first-poss_ele[left].first);
+            }
+            else
+            {
+                break;
+            }
+        }
+        return res;
+    
+
+    }
+};
