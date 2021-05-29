3cc48b426701fe013f818140c147305aa8c261b6
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-05-29 20:16:29 +0530

    Create single_inheritance.cpp

---
+++ OOPS/Inheritance/single_inheritance.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+ 
+class Base
+{
+    int data1; // private member
+    public:
+        int data2;
+        void setData();
+        int getData1();
+        int getData2();
+};
+
+void Base :: setData()
+{
+    data1 = 5;
+    data2 = 4;
+}
+
+int Base :: getData1()
+{
+    return data1;
+}
+
+int Base :: getData2()
+{
+    return data2;
+}
+
+// derived class inherits from base class
+// public implies that public data of base class is inherited to public data of derived class
+class Derived : public Base
+{
+    public:
+        int data3;
+        void display();
+        void setData3();
+};
+
+void Derived :: display()
+{
+    cout << "data1 is " << getData1() << endl;
+    cout << "data2 is " << data2 << endl;
+    cout << "data3 is " << data3 << endl; 
+}
+
+
+void Derived :: setData3()
+{
+    data3 = data2 * getData1();
+}
+
+int main()
+{
+    Derived drvd;
+    drvd.setData();
+    drvd.setData3();
+    drvd.display();
+    return 0;
+}
