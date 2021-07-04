19cf56a603ee59089692e57f7d069f3ecd20b2a1
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-07-04 12:34:50 +0530

    Create count_0_1_2_s.cpp

---
+++ FINAL450/count_0_1_2_s.cpp
@@ -0,0 +1,89 @@
+// https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+// Directly using counts and updating the array.
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0;i<n;i++)
+        {
+            if(a[i] == 0)
+            {
+                count0++;
+            }
+            else if(a[i] == 1)
+            {
+                count1++;
+            }
+            else
+            {
+                count2++;
+            }
+        }
+        int i = 0;
+        while(i<n)
+        {
+            while(count0)
+            {
+                a[i] = 0;
+                i++;
+                count0--;
+            }
+            while(count1)
+            {
+                a[i] = 1;
+                i++;
+                count1--;
+            }
+            while(count2)
+            {
+                a[i] = 2;
+                i++;
+                count2--;
+            }
+        }
+    }
+    
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
