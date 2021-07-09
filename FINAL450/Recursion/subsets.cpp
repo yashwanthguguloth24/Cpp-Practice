134f0d9b0edf5ecdafecca23fd6d5cbfb34d2ee0
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-07-09 15:41:38 +0530

    Update subsets.cpp

--- FINAL450/Recursion/subsets.cpp
+++ FINAL450/Recursion/subsets.cpp
@@ -1,16 +1,13 @@
 // https://leetcode.com/problems/subsets/
 
 /*
-Using backtracking with the following steps
-1.Make decision - for ith index take element in v2,donot take element v3
-2.Recurr - recur using v2 and v3
-3.Undo - property is not destroyed to undo
+Using recursion to take or not take.
 */
 
 class Solution {
 public:
     vector<vector<int>> ans;
-    void subsetsBacktrack(vector<int>& nums,int i,vector<int>v1)
+    void subsetsRec(vector<int>& nums,int i,vector<int>v1)
     {
         if(i>=nums.size())
         {
@@ -19,15 +16,15 @@ public:
         }
         vector<int> v2 = v1;// to take
         vector<int> v3 = v1;// to not take
-        v2.push_back(nums[i]); //make decision
-        subsetsBacktrack(nums,i+1,v2); //recur
-        subsetsBacktrack(nums,i+1,v3); // recur
+        v2.push_back(nums[i]);
+        subsetsRec(nums,i+1,v2); //recur
+        subsetsRec(nums,i+1,v3); // recur
     }
     
     
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> v1;
-        subsetsBacktrack(nums,0,v1);
+        subsetsRec(nums,0,v1);
         return ans;
     }
 };
