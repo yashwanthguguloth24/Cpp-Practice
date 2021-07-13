70a235940ac4f75a3235f0cb38cc9845744bd2af
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-07-13 19:54:50 +0530

    Create min_stack_gfg.cpp

---
+++ FINAL450/Stacks and Queues/min_stack_gfg.cpp
@@ -0,0 +1,86 @@
+// https://practice.geeksforgeeks.org/problems/special-stack/1#
+
+// Direct using push,pop operations. For minimum pop all the elements and check.
+
+// { Driver Code Starts
+#include<iostream>
+#include<stack>
+using namespace std;
+void push(stack<int>& s,int a);
+bool isFull(stack<int>& s,int n);
+bool isEmpty(stack<int>& s);
+int pop(stack<int>& s);
+int getMin(stack<int>& s);
+//This is the STL stack (http://quiz.geeksforgeeks.org/stack-container-adaptors-the-c-standard-template-library-stl/).
+stack<int> s;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,a;
+		cin>>n;
+		while(!isEmpty(s)){
+		    pop(s);
+		}
+		while(!isFull(s,n)){
+			cin>>a;
+			push(s,a);
+		}
+		cout<<getMin(s)<<endl;
+	}
+}// } Driver Code Ends
+
+
+void push(stack<int>& s, int a){
+	// Your code goes here
+	s.push(a);
+}
+
+bool isFull(stack<int>& s,int n){
+	// Your code goes here
+	bool flag = false;
+	if(s.size() == n)
+	{
+	    flag = true;
+	}
+	return flag;
+}
+
+bool isEmpty(stack<int>& s){
+	// Your code goes here
+	if(s.size() == 0)
+	{
+	    return true;
+	}
+	return false;
+}
+
+int pop(stack<int>& s){
+	// Your code goes here
+	if(s.size() != 0)
+	{
+	    int ans = s.top();
+	    s.pop();
+	    return ans;
+	}
+	return -1;
+	
+}
+
+int getMin(stack<int>& s){
+	// Your code goes here
+	int min_val = 1000000;
+	while(s.size() != 0)
+	{
+	    int temp = s.top();
+	    if(temp < min_val)
+	    {
+	        min_val = temp;
+	    }
+	    s.pop();
+	}
+	return min_val;
+	
+	
+}
+
