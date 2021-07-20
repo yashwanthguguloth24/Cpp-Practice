7fca12dee2e7934cc5006f1651b9fe1a86f60ece
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-07-21 01:28:27 +0530

    Create mirror_Invert_a_binary_tree.cpp

---
+++ FINAL450/Binary Tree/mirror_Invert_a_binary_tree.cpp
@@ -0,0 +1,27 @@
+// https://www.geeksforgeeks.org/create-a-mirror-tree-from-the-given-binary-tree/
+// https://leetcode.com/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)
+        {
+            return root;
+        }
+        TreeNode* temp = invertTree(root->left);
+        root->left = invertTree(root->right);
+        root->right = temp;
+        return root;
+    }
+};
