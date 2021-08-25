c6f58c7c4d78139352ae147c6cbd1814cab2a2ed
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-08-25 15:45:19 +0530

    Update trapping_rain_water.cpp

--- FINAL450/trapping_rain_water.cpp
+++ FINAL450/trapping_rain_water.cpp
@@ -39,3 +39,47 @@ class Solution{
     }
 };
 
+// ************** USING LEFTMAX AND RIGHT OF A INDEX ******************************
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    int trappingWater(int arr[], int n){
+        int max_ind_left = 0;
+        vector<int> max_left(n);
+        for(int i = 0;i<n;i++)
+        {
+            // if(arr[max_ind_left] < arr[i])
+            // {
+            //     max_ind_left = i;
+            // }
+            max_left[i] = max_ind_left;
+            if(arr[max_ind_left] < arr[i])
+            {
+                max_ind_left = i;
+            }
+        }
+        int max_ind_right = n-1;
+        vector<int> max_right(n);
+        for(int i = n-1;i>=0;i--)
+        {
+            max_right[i] = max_ind_right;
+            if(arr[max_ind_right] < arr[i])
+            {
+                max_ind_right = i;
+            }
+            // max_right[i] = max_ind_right;
+        }
+        
+        int ans = 0;
+        for(int i = 0;i<n;i++)
+        {
+            if(min(arr[max_right[i]],arr[max_left[i]]) > arr[i])
+            {
+                ans += min(arr[max_right[i]],arr[max_left[i]])-arr[i];   
+            }
+        }
+        return ans;
+        
+    }
+};
