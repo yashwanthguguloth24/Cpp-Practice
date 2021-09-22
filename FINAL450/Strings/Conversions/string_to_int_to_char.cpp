de9fc96792345ca37eeea9e1bee288950c77ce13
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-09-22 15:52:30 +0530

    Create string_to_int_to_char.cpp

---
+++ FINAL450/Strings/Conversions/string_to_int_to_char.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    char c = '4';
+    int val = c-'0';
+    cout << val << endl;
+
+    string s = "234";
+    int ans = stoi(s);
+    cout << ans << endl;
+
+    int a = 456;
+    string r = to_string(a);
+    cout << r << endl;
+
+    int b = 3;
+    char t = '0'+b;
+    cout << t << endl;
+
+    return 0;
+}
