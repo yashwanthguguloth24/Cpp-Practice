fd6703552b2988eae56dfe222f4f7badcdfca6b4
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2021-10-20 23:31:46 +0530

    Create ops_to_make_sum_rows_cols_equal.cpp

---
+++ Past Ques/zomato/ops_to_make_sum_rows_cols_equal.cpp
@@ -0,0 +1,48 @@
+// https://practice.geeksforgeeks.org/problems/make-matrix-beautiful-1587115620/1
+
+
+class Solution
+{
+    public:
+    //Function to find minimum number of operations that are required 
+    //to make the matrix beautiful.
+    int findMinOpeartion(vector<vector<int> > matrix, int n)
+    {
+        // code here
+        vector<int> row_sum(n,0);
+        vector<int> col_sum(n,0);
+        
+        int mx_sum = INT_MIN;
+        
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                row_sum[i] += matrix[i][j];
+                col_sum[j] += matrix[i][j];
+            }
+        }
+        
+        for(int i = 0;i<n;i++)
+        {
+            mx_sum = max(mx_sum,max(row_sum[i],col_sum[i]));
+        }
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        while(i < n && j < n)
+        {
+            int df = mx_sum-min(row_sum[i],col_sum[j]);
+            matrix[i][j] += df;
+            row_sum[i] += df;
+            col_sum[j] += df;
+            ans += df;
+            if(row_sum[i] == mx_sum) i++;
+            
+            if(col_sum[j] == mx_sum) j++;
+        }
+        return ans;
+        
+        
+    } 
+};
